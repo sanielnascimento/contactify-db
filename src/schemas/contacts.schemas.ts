fbe9bf73113d8b48b6577dbe76609af19290079a
Saniel1990 <saniel_sabr@outlook.com>
2023-06-03 09:16:01 -0300

    feat(schemas): add contacts schemas

---
+++ src/schemas/contacts.schemas.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+const contactSchema = z.object({
+  id: z.string(),
+  name: z.string().min(6).max(45),
+  email: z.string().email().min(10).max(45),
+  phone: z.number().min(10).max(14),
+  comment: z.string().max(300).nullable(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+  deletedAt: z.string().nullable(),
+});
+
+const contactCreateSchema = contactSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+  deletedAt: true,
+});
+
+const contactUpdateSchema = contactCreateSchema.partial();
+const userResponseSchema = contactSchema;
