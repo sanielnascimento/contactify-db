38dd330a622158e1ba32da8687fcc7fc7ce3ac0d
Saniel1990 <saniel_sabr@outlook.com>
2023-07-08 11:59:03 -0300

    fix: excluding ssl configuration and logs

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,7 @@ import { AppDataSource } from "./data-source";
 import "dotenv/config";
 
 const PORT: number = Number(process.env.APP_PORT!);
-console.log(`Value of APP_PORT: ${process.env.APP_PORT}`);
-console.log(`Parsed value of PORT: ${PORT}`);
-
-const runningMessage: string = `Server is running on port http://localhost:${PORT}`;
+const runningMessage: string = `Server is running on port:${PORT}`;
 
 AppDataSource.initialize()
   .then(() => {
